8a9a3b35a26e7bf4386ac40500ade093f8ba5327
leofab <leofabr3@gmail.com>
2023-05-16 11:06:41 -0300

    initial config of web && mobile with tailwind

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -3,18 +3,9 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View className="bg-zinc-800 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 font-bold text-5xl">My First ReactNative App</Text>
+      <StatusBar style="inverted" />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
