77789d4e62edd2d8413ea1604241da4dcadd6cfa
leofab <leofabr3@gmail.com>
2023-05-17 11:52:35 -0300

    stripes and lucide importing and implementing

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,4 +1,4 @@
-
+import { User } from 'lucide-react';
 export default function Home() {
   return <main className="grid grid-cols-2 min-h-screen">
     {/* Esquerda */}
@@ -7,6 +7,13 @@ export default function Home() {
       <div className="absolute right-0 top-1/2 h-[288px] w-[526px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purple-700 opacity-50 blur-full" />
       {/* Stripes */}
       <div className="absolute bottom-0 right-2 top-0 w-2 bg-stripes"/>
+
+      <a href="" className="flex items-center gap-3 text-left">
+        <div className="flex h-10 w-10 items-center justify-center rounded-full bg-gray-400">
+          <User className="h-5 w-5 text-gray-500" />
+        </div>
+        <p><span>Crie sua conta e guarde suas memórias</span></p>
+      </a>
     </div>
     {/* Direita */}
     <div className="flex flex-col p-16 bg-[url(../assets/bg-stars.svg)] bg-cover">
