829d4be666e7d670bad9e3b91c4626ff2cc3ac4a
leofab <leofabr3@gmail.com>
2023-05-18 15:19:37 -0300

    zod return by id with validation

--- server/src/routes/memories.ts
+++ server/src/routes/memories.ts
@@ -1,5 +1,6 @@
 import {prisma} from "../lib/prisma";
 import {FastifyInstance} from "fastify";
+import z from "zod";
 export async function memoriesRoutes(app: FastifyInstance) {
 
   app.get('/memories', async () => {
@@ -16,9 +17,17 @@ export async function memoriesRoutes(app: FastifyInstance) {
       }
     });
   });
-  app.get('/memories/:id', async () => {
-    const users = await prisma.user.findMany();
-    return users;
+
+  app.get('/memories/:id', async (request) => {
+    const paramSchema = z.object({
+      id: z.string().uuid(),
+    });
+    const {id} = paramSchema.parse(request.params);
+
+    const memory = await prisma.memory.findUniqueOrThrow({
+      where: {
+        id,
+      },
   });
   app.post('/memories', async () => {
     const users = await prisma.user.findMany();
