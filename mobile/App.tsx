8d8e9b040dbd6bc8f84c8850f561dd4595abdebc
leofab <leofabr3@gmail.com>
2023-05-18 13:30:34 -0300

    new view text and touchable implement

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import {Image, ImageBackground, View} from 'react-native';
+import {Image, ImageBackground, View, Text, TouchableOpacity} from 'react-native';
 import blurBg from './src/assets/bg-blur.png'
 import Stripes from './src/assets/stripes.svg'
 import memorySpaces from './src/assets/memoryspaces.png';
@@ -29,12 +29,25 @@ export default function App() {
   return (
     <ImageBackground
       source={blurBg}
-      className="relative bg-zinc-800 flex-1 items-center"
+      className="relative bg-zinc-800 flex-1 items-center px-8"
       imageStyle={{ position: 'absolute', left: '-100%' }}
     >
       <StyledStripes className="absolute left-2"/>
       <View className="flex-1 items-center justify-center gap-6">
       <Image source={memorySpaces}/>
+        <View className="space-y-2 items-center justify-center">
+          <Text className="text-gray-50 font-title text-2xl leading-tight font-bold">
+            Suas memórias em um só lugar
+          </Text>
+          <Text className="text-gray-100 font-body text-base leading-relaxed">
+            Colecione momentos, fotos, vídeos e muito mais.
+          </Text>
+        </View>
+        <TouchableOpacity className="rounded-full bg-blue-400 px-5 py-3">
+          <Text className="font-alt text-sm uppercase text-black">
+            Cadastrar Lembrança
+          </Text>
+        </TouchableOpacity>
       </View>
       <StatusBar style="inverted" />
     </ImageBackground>
