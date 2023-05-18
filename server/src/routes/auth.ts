34edc669f31f636e2c14b5e6e594c6dfa6f6ee82
leofab <leofabr3@gmail.com>
2023-05-18 18:31:53 -0300

    axios and auth routes implement

---
+++ server/src/routes/auth.ts
@@ -0,0 +1,12 @@
+import {FastifyInstance} from "fastify";
+import z from "zod";
+
+export async function authRoutes(app: FastifyInstance){
+  app.post('/register', async (request)=>{
+    const bodySchema = z.object({
+      code: z.string(),
+    })
+
+    const { code } = bodySchema.parse(request.body);
+  })
+}
