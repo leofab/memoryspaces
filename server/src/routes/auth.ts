f60f9e33f7f845db233a5fa28a310c0b7c5c1a35
leofab <leofabr3@gmail.com>
2023-05-18 18:51:26 -0300

    register route user validation

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import {FastifyInstance} from "fastify";
 import axios from "axios";
 import z from "zod";
+import {prisma} from "../lib/prisma";
 
 export async function authRoutes(app: FastifyInstance) {
   app.post('/register', async (request) => {
@@ -35,7 +36,24 @@ export async function authRoutes(app: FastifyInstance) {
       avatar_url: z.string().url(),
     })
 
-    const user = userSchema.parse(userResponse.data);
+    const userInfo = userSchema.parse(userResponse.data);
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubID: userInfo.id,
+      }
+    })
+
+    if(!user) {
+      user = await prisma.user.create({
+        data: {
+          githubID: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.name,
+          avatarURL: userInfo.avatar_url,
+        },
+      })
+    }
 
 
     return access_token;
