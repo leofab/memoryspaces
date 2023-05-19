75420ede700e60862fb6d051151ab91874a94481
leofab <leofabr3@gmail.com>
2023-05-19 12:42:51 -0300

    token finnaly persisting in frontend

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest){
   const registerResponse = await api.post('/register', {
     code,
   })
-  const {token} = registerResponse.data;
+  const token = registerResponse.data;
   console.log(token);
 
   const redirectURL = new URL ('/', request.url)
